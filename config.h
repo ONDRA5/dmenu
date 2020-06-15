85834bb3e8302dac6f4354d237b214346839f3fb
ONDRA5 <ondra.halama@gmail.com>
2020-06-15 21:20:51 +0200

    squashed af, accidentally deleted the .git folder lol

---
+++ config.h
@@ -0,0 +1,26 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Hack Nerd Font:size=10",
+	"JoyPixels:pixelsize=12:antialias=true:autohint=true"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#ebdbb2", "#282828" },
+	[SchemeSel] = { "#ebdbb2", "#98971a" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+	[SchemeSelHighlight] = { "#282828", "#FFD7AF" },
+	[SchemeNormHighlight] = { "#282828", "#D79921" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
